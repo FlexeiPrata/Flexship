586c05385a4048829a2c467a51367cca689b14ec
Degidrant <SatelliteHyperion@gmail.com>
2021-07-03 14:37:24 +0300

    test ship commit

--- app/src/main/java/com/flexship/flexshipcookingass/MainActivity.kt
+++ app/src/main/java/com/flexship/flexshipcookingass/MainActivity.kt
@@ -2,10 +2,19 @@ package com.flexship.flexshipcookingass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        //Zalupus
     }
 }
