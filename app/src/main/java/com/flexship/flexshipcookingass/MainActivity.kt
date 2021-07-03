ccab92896ab834a3ccd34ab1bd5b725d834f4fa3
Separd-ui <sasha.sepelenco@mail.ru>
2021-07-03 16:49:24 +0300

    +Icons

--- app/src/main/java/com/flexship/flexshipcookingass/MainActivity.kt
+++ app/src/main/java/com/flexship/flexshipcookingass/MainActivity.kt
@@ -3,13 +3,24 @@ package com.flexship.flexshipcookingass
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.navigation.fragment.NavHostFragment
+import com.flexship.flexshipcookingass.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController=navHostFragment.navController
+
     }
+
+
 }
--- app/src/main/java/com/flexship/flexshipcookingass/other/Constans.kt
+++ app/src/main/java/com/flexship/flexshipcookingass/other/Constans.kt
@@ -2,10 +2,8 @@ package com.flexship.flexshipcookingass.other
 
 object Constans {
 
-<<<<<<< HEAD
     const val DATABASE_NAME="cook_database"
     const val SHARED_PREF_NAME="PREFS"
-=======
 
->>>>>>> 0070fc1d10019a8265d591919906bce832c29db7
+
 }
